053f5ad7c2099a2289364a63075575f2db148e21
Your Name <rodrigo.medranoorta@umg.edu.mx>
2024-10-14 23:11:59 -0600

    first commit

--- src/views/SprintDetalle.jsx
+++ src/views/SprintDetalle.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import { Box, Heading, Text, Stack, Checkbox, Input, Button, Select } from '@chakra-ui/react';
 import { useParams, useNavigate } from 'react-router-dom';
